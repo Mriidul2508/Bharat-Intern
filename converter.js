f2d6f8149a88c2f55ab4dcdf835876bc93550cc1
Mriidul2508 <113883038+Mriidul2508@users.noreply.github.com>
2023-10-29 13:16:18 +0530

    Temperature Converter
    Temperature converter website based on html and css.

---
+++ converter.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const celsiusInput = document.getElementById("celsius");
+    const fahrenheitInput = document.getElementById("fahrenheit");
+    const convertButton = document.getElementById("convert");
+
+    convertButton.addEventListener("click", function() {
+        const celsius = parseFloat(celsiusInput.value);
+        if (!isNaN(celsius)) {
+            const fahrenheit = (celsius * 9/5) + 32;
+            fahrenheitInput.value = fahrenheit.toFixed(2);
+        } else {
+            alert("Please enter a valid number for Celsius.");
+        }
+    });
+});
